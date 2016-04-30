655b045ea30b81558769a9db06cab265c22c4ae4
rajesh dhiman <rajeshdhimanskt@gmail.com>
2016-04-30 11:44:26 +0530

    logging events

--- index.js
+++ index.js
@@ -26,8 +26,10 @@ app.get('/webhook', function (req, res) {
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
+    console.log(events);
     for (i = 0; i < events.length; i++) {
         var event = events[i];
+        console.log(event);
  if (event.message && event.message.text) {
     if (!kittenMessage(event.sender.id, event.message.text)) {
         sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
